f00c45e9bd8d6b4d5acad3242275475243047d86
miiizul <1049599771@qq.com>
2019-12-17 21:09:01 +0800

    2 pai

--- static/js/login.js
+++ static/js/login.js
@@ -1,23 +1,24 @@
 $("#loginBtn").click(function(){
+	event.preventDefault();
 	$.ajax({
 		type:"POST",
 		url:"/user/login",
 		data: {
 			username:  $("#username").val(),
 			password: $("#passowrd").val(),
-			flag: true
 		},
 		success: function(response){
-			
 			jsonobj = response;
 			if(jsonobj.resultCode=="10001")
 			{
 				//登录成功
+				alert("登录成功")
 	    		window.location.href = "/crawlPage";
-			}	
+			}
 			else
 			{
 				//登录失败
+				alert(jsonobj.resultDesc)
 				console.log(jsonobj.resultDesc);
 			}
 		},
--- user/views.py
+++ user/views.py
@@ -1,23 +1,54 @@
+import re
+
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
 from django.http.response import JsonResponse
+from user import models
 # Create your views here.
 
 def login(request):
     print('进入接口login')
     json = {}
     if request.method == "POST":
-        username = request.POST.get("username", None)
-        password = request.POST.get("password", None)
-        flag = request.POST.get("flag", None)
+        username = request.POST.get("username")
+        password = request.POST.get("password")
 
-        if flag:
-            json['resultCode'] = '10001'
-            json['resultDesc'] = '登录成功'
-            print(username, password, flag)
+        if username.strip() == '' or password.strip() == '':
+            json['resultCode'] = '20000'
+            json['resultDesc'] = '参数不全'
         else:
-            json['resultCode'] = '50000'
-            json['resultDesc'] = '登录失败'
-            print('fail')
+            phoneFlag = True
+            emailFlag = True
+            try:
+                user = models.User.objects.get(u_phone=username)
+            except:
+                phoneFlag = False
+
+            if phoneFlag:
+                if user.u_password == password:
+                    json['resultCode'] = '10001'
+                    json['resultDesc'] = '登陆成功'
+                    request.session['u_id'] = user.u_id
+                else:
+                    json['resultCode'] = '10003'
+                    json['resultDesc'] = '密码错误'
+            else:
+                try:
+                    user = models.User.objects.get(u_email=username)
+                except:
+                    emailFlag = False
+
+                if emailFlag:
+                    print("邮箱存在")
+                    if user.u_password == password:
+                        json['resultCode'] = '10001'
+                        json['resultDesc'] = '登陆成功'
+                        request.session['u_id'] = user.u_id
+                    else:
+                        json['resultCode'] = '10003'
+                        json['resultDesc'] = '密码错误'
+                else:
+                    json['resultCode'] = '10002'
+                    json['resultDesc'] = '用户不存在'
 
     return JsonResponse(json)
